6ff465ff8625f18b1a61af792ab7f46fa1f0c090
cmlagant <cmlagant@gmail.com>
2024-12-04 21:59:47 -0500

    Outlined problem in psuedocode

--- 124.py
+++ 124.py
@@ -1,2 +1,24 @@
 def main():
-    
+    # Import txt - split on /n
+    test =[
+        ["MMMSXXMASM"],
+        ["MSAMXMSMSA"],
+        ["AMXSXMAAMM"],
+        ["MSAMASMSMX"],
+        ["XMASAMXAMM"],
+        ["XXAMMXXAMA"],
+        ["SMSMSASXSS"],
+        ["SAXAMASAAA"],
+        ["MAMMMXMMMM"],
+        ["MXMXAXMASX"]
+    ]
+    # Reorient Strings - Vertical. Loop through test and build a new list of [all test[0], all test[1], etc.]
+
+    # Reorient Strings - Diagonal. Loop through test and build a new list of valid [test1[0], test2[1], test3[2], etc.]
+
+    # Reorient Strings - Reversed. Reverse Order all existing variations created so far
+
+    # Count Occurences of "XMAS" in each string in all lists created above
+
+if __name__ == "__main__":
+    main()
