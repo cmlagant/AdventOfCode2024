2d114d9cbb883900639ebaaaeaca91bb5a8bbf26
cmlagant <cmlagant@gmail.com>
2024-12-05 21:01:06 -0500

    Started diag() function

--- 124.py
+++ 124.py
@@ -19,13 +19,12 @@ def main():
     print(vert_data)
     # Reorient Strings - Diagonal. Loop through test and build a new list of valid [test1[0], test2[1], test3[2], etc.]
     diag_data = diag(test)
-    # Reorient Strings - Reversed. Reverse Order all existing variations created so far
-    flattened_data = flatten(test, vert_data, diag_data)
-    reversed_data = reverse(flattened_data)
-    data = flattened_data.append(reversed_data)
+    print(diag_data)
+    # Reorient Strings - Reversed. Reverse Order all existing variations created so far \\ No need. Instead just findall "SAMX" too
+    #flattened_data = flatten(test, vert_data, diag_data)
     # Count Occurences of "XMAS" in each string in all lists created above
-    matches = re.findall("XMAS", data)
-    print(f"XMAS appears {len(matches)} times in the puzzle")
+    #matches = re.findall("XMAS", data)
+    #print(f"XMAS appears {len(matches)} times in the puzzle")
 
 def vert(li):
     vertical_strings = []
@@ -35,6 +34,20 @@ def vert(li):
             vertical_string += i[j]
         vertical_strings.append(vertical_string)
     return vertical_strings
+
+def diag(li):
+    diagonal_strings = []
+    for col in (range(len(li[0])-1)):        # For every Column
+        diagonal_string = ""
+        i = 0
+        for row in li:
+            diagonal_string += row[col+i]
+            i += 1
+        diagonal_strings.append(diagonal_string)
+    return diagonal_strings
+
+
+
 
 
 if __name__ == "__main__":
